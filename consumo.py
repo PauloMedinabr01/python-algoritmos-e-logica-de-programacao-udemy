28c16c0bfadd370ee0f2b038aac9719876a9476a
PauloMedinabr01 <paulomedinabr01@gmail.com>
2022-11-20 15:45:22 -0300

    Alguns ajustes

---
+++ consumo.py
@@ -0,0 +1,11 @@
+distancia: int
+gasto: float
+consumo: float
+
+distancia = int(input("Distancia percorrida: "))
+
+gasto = float(input("Combustivel gasto: "))
+
+consumo = distancia/gasto
+
+print(f"Consumo medio = {consumo:.3f}")
